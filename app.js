26427daf6b3664dc007ed28b18c1777f91221fa6
jasatirtaluhur-biki <jtirtaluhur@gmail.com>
2026-04-16 17:05:51 +0700

    Update app.js
    Signed-off-by: jasatirtaluhur-biki <jtirtaluhur@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==========================================
 // KONFIGURASI API
 // ==========================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbySd3Od-R486x9eY8igVUSF0RXobU_ElC5b15WAGxLjMnNvsM2RHcy-XW7pBGY66ntt/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxsnTaTUMpjKxCWRT3wN0TaALFYedxAvflfZ_v99v7NpkhQBwxkMJBWJvLvTXXkpcJg/exec';
 
 // ==========================================
 // STATE MANAGEMENT
